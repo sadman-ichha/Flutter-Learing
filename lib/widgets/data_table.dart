434c14d863a12b16bffefd3ec45fa574eff14efc
sadman_ichha <sadmanichha1@gmail.com>
2022-10-27 10:51:16 +0600

    commit

--- lib/widgets/data_table.dart
+++ lib/widgets/data_table.dart
@@ -1,36 +1,56 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class DataTableWidget extends StatelessWidget {
+  String schoolName = "ABC School";
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: dataTable().studentDetails,
+        body: StudentData.bodyData(),
       ),
     );
   }
 }
 
-String schoolName = "ABC School";
+class StudentData {
+  // pass data one page to another page via Constructors
+  String? name;
+  int? age;
+  String? gender;
+  String schoolName;
 
-class dataTable {
-  var studentDetails = DataTable(columns: [
-    DataColumn(label: Text("Name"), numeric: true),
-    DataColumn(label: Text("Age")),
-    DataColumn(label: Text("Gender")),
-    DataColumn(label: Text("School")),
-  ], rows: [
-    DataRow(cells: [
-      DataCell(Text("Sadman")),
-      DataCell(Text("40")),
-      DataCell(Text("Male")),
-      DataCell(Text(schoolName)),
-    ]),
-    DataRow(cells: [
-      DataCell(Text("Ichha")),
-      DataCell(Text("60")),
-      DataCell(Text("Male")),
-      DataCell(Text(schoolName)),
-    ]),
-  ]);
+  StudentData(
+      {required this.name,
+      required this.age,
+      required this.gender,
+      this.schoolName = "ABC School"});
+
+  //Constructors end..    
+
+  static Widget bodyData() {
+    List<StudentData> names = [
+      StudentData(name: "Sadman", age: 30, gender: "Male"),
+      StudentData(name: "Rahul", age: 20, gender: "Male"),
+      StudentData(name: "Zerin", age: 25, gender: "Female"),
+    ];
+    return DataTable(
+        columns: [
+          DataColumn(label: Text("Name")),
+          DataColumn(label: Text("Age")),
+          DataColumn(label: Text("Gender")),
+          DataColumn(label: Text("School")),
+        ],
+        rows: names
+            .map(
+              (student) => DataRow(cells: [
+                DataCell(Text(student.name.toString())),
+                DataCell(Text(student.age.toString())),
+                DataCell(Text(student.gender.toString())),
+                DataCell(Text(student.schoolName.toString())),
+              ]),
+            )
+            .toList());
+  }
 }
