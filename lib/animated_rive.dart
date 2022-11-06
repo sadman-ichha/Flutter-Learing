c04aa7e25190db78ec402d07fffb6f55c5e4efcb
sadman_ichha <sadmanichha1@gmail.com>
2022-11-06 13:50:09 +0600

    app icons chance

--- lib/animated_rive.dart
+++ lib/animated_rive.dart
@@ -1,22 +1,39 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-class RiveAnimated extends StatelessWidget {
-  const RiveAnimated({Key? key}) : super(key: key);
+class RiveAnimated extends StatefulWidget {
+  @override
+  State<RiveAnimated> createState() => _RiveAnimatedState();
+}
 
+class _RiveAnimatedState extends State<RiveAnimated> {
+  bool _value = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          height: 400.0,
-          width: 300.0,
-          child: RiveAnimation.asset(
-            "assets/animation/mixing-animations.riv",
-            animations: ["Expert", "Beginner", 'Intermediate'],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400.0,
+              width: 300.0,
+              child: RiveAnimation.asset(
+                "assets/animation/mixing-animations.riv",
+                animations: [_value == false ? 'Intermediate' : "Beginner"],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _value = !_value;
+                });
+              },
+              child: Text("Beginner"),
+            ),
+          ],
         ),
       ),
     );
