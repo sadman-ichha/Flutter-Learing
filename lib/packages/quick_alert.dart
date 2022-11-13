a44b2f172345ada9312598e4952e2fd787e53d5e
sadman_ichha <sadmanichha1@gmail.com>
2022-11-13 22:00:39 +0600

    added

--- lib/packages/quick_alert.dart
+++ lib/packages/quick_alert.dart
@@ -7,25 +7,20 @@ import 'package:quickalert/widgets/quickalert_dialog.dart';
 class QuickAlertPackages extends StatelessWidget {
   const QuickAlertPackages({Key? key}) : super(key: key);
 
-  showAlert(BuildContext context, QuickAlertType alertType, String text) {
-    QuickAlert.show(
-      context: context,
-      type: alertType,
-      text: text,
-    );
-  }
-
   Card customButtom(
-      {required onTap, required String title, required String text}) {
+      {required Widget title, required String text, required onTap}) {
     return Card(
       shape: StadiumBorder(),
       margin: EdgeInsets.symmetric(horizontal: 30.0),
+      clipBehavior: Clip.antiAlias,
       elevation: 5,
       child: ListTile(
-        onTap: onTap,
-        title: Text(title),
-        subtitle: Text(text),
-      ),
+          title: title,
+          subtitle: Text(text),
+          onTap: onTap,
+          trailing: Icon(
+            Icons.keyboard_arrow_right,
+          )),
     );
   }
 
@@ -39,21 +34,76 @@ class QuickAlertPackages extends StatelessWidget {
         elevation: 9,
         backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            customButtom(
-                onTap: () {},
-                title: "Success",
-                text: "Transaction Completed Successfully!"),
-            SizedBox(height: 10),
-            customButtom(
-                onTap: () {},
-                title: "Error",
-                text: "Sorry, something went wrong"),
-          ],
-        ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 30.0),
+        children: [
+          customButtom(
+            title: Text("Success"),
+            text: "Transaction Completed Successfully!",
+            onTap: () {
+              QuickAlert.show(
+                  context: context,
+                  type: QuickAlertType.success,
+                  text: "Transaction Completed Successfully!");
+            },
+          ),
+          SizedBox(height: 10),
+          customButtom(
+              title: Text("Error"),
+              text: 'Sorry, something went wrong',
+              onTap: () {
+                QuickAlert.show(
+                    context: context,
+                    type: QuickAlertType.error,
+                    title: 'Oops...',
+                    text: 'Sorry, something went wrong');
+              }),
+          SizedBox(height: 10),
+          customButtom(
+              title: Text("Warning"),
+              text: 'You just broke protocol',
+              onTap: () {
+                QuickAlert.show(
+                    context: context,
+                    type: QuickAlertType.warning,
+                    text: 'You just broke protocol');
+              }),
+          SizedBox(height: 10),
+          customButtom(
+              title: Text("Info"),
+              text: 'Buy two, get one free',
+              onTap: () {
+                QuickAlert.show(
+                  context: context,
+                  type: QuickAlertType.info,
+                  text: 'Buy two, get one free',
+                );
+              }),
+          SizedBox(height: 10),
+          customButtom(
+              title: Text("Confirm"),
+              text: 'Do you want to logout',
+              onTap: () {
+                QuickAlert.show(
+                    context: context,
+                    type: QuickAlertType.confirm,
+                    confirmBtnText: "Okey",
+                    cancelBtnText: "Cancel",
+                    confirmBtnColor: Colors.green,
+                    text: 'Sorry, something went wrong');
+              }),
+          SizedBox(height: 10),
+          customButtom(
+              title: Text("Loading"),
+              text: 'Fetching your data',
+              onTap: () {
+                QuickAlert.show(
+                  context: context,
+                  type: QuickAlertType.loading,
+                  text: 'Fetching your data',
+                );
+              }),
+        ],
       ),
     );
   }
