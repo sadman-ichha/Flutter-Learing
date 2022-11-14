5f9fbd56502c292eb063262466b0e5ee2d84c0fc
sadman_ichha <sadmanichha1@gmail.com>
2022-11-14 12:28:37 +0600

    validated

--- lib/TextForm_Validation/textform_validation.dart
+++ lib/TextForm_Validation/textform_validation.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: prefer_final_fields, prefer_const_constructors, unused_field, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
 class TextFormValidation extends StatelessWidget {
-  const TextFormValidation({Key? key}) : super(key: key);
-
+  final _formlKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,31 +14,55 @@ class TextFormValidation extends StatelessWidget {
         title: Text("TextFormValidation",
             style: Theme.of(context).textTheme.bodyMedium),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 0.0),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 0.0),
+        child: Form(
+          key: _formlKey,
           child: Column(
             children: [
               TextFormField(
-                  decoration: InputDecoration(
-                hintText: "Enter your Name?",
-                hintStyle: Theme.of(context).textTheme.bodyMedium,
-              )),
+                decoration: InputDecoration(
+                  hintText: "Enter your Name?",
+                  hintStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "this field can't be empty";
+                  }
+                },
+              ),
               TextFormField(
-                  decoration: InputDecoration(
-                hintText: "Enter your Email?",
-                hintStyle: Theme.of(context).textTheme.bodyMedium,
-              )),
+                decoration: InputDecoration(
+                    hintText: "Enter your Email?",
+                    hintStyle: Theme.of(context).textTheme.bodyMedium),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "this field can't be empty";
+                  } else if (value.length < 3) {
+                    return "enter a validate email";
+                  }
+                },
+              ),
               TextFormField(
-                  decoration: InputDecoration(
-                hintText: "Enter your Password!",
-                hintStyle: Theme.of(context).textTheme.bodyMedium,
-              )),
+                decoration: InputDecoration(
+                    hintText: "Enter your Password!",
+                    hintStyle: Theme.of(context).textTheme.bodyMedium),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "this field can't be empty";
+                  } else if (value.length < 6) {
+                    return "password must be 6 characters";
+                  }
+                },
+              ),
               SizedBox(height: 40.0),
               SizedBox(
                   width: 300.0,
                   child: ElevatedButton(
-                      onPressed: () {}, child: Text("Validate Now"))),
+                      onPressed: () {
+                        _formlKey.currentState!.validate();
+                      },
+                      child: Text("Validate Now"))),
             ],
           ),
         ),
