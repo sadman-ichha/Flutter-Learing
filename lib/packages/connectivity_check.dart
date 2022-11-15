ba95244bd9c078513b60c9555ce358d0f2dba23a
sadman_ichha <sadmanichha1@gmail.com>
2022-11-15 11:17:41 +0600

    check Connect

--- lib/packages/connectivity_check.dart
+++ lib/packages/connectivity_check.dart
@@ -1,14 +1,42 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
 class ConnectivityCheck extends StatelessWidget {
-  const ConnectivityCheck({Key? key}) : super(key: key);
+  StreamSubscription? subscription;
+
+  Future checkConnection(context) async {
+    var connectivityRes = await Connectivity().checkConnectivity();
+    if (connectivityRes == ConnectivityResult.mobile) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Connected to mobile network")),
+      );
+    } else if (connectivityRes == ConnectivityResult.wifi) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Connected to Wifi network")));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("No Internet")));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Connectivity Check",
+            style: Theme.of(context).textTheme.titleLarge),
+        centerTitle: true,
+        elevation: 9,
+        backgroundColor: Colors.white,
+      ),
       body: Center(
-        child: ElevatedButton(onPressed: (){}, child: Text("Check")),
+        child: ElevatedButton(
+            onPressed: () {
+              checkConnection(context);
+            },
+            child: Text("Check")),
       ),
     );
   }
