114dff6aac150557207781998f41542bc1768a5b
sadman_ichha <sadmanichha1@gmail.com>
2022-11-29 20:35:13 +0600

    https

--- lib/API Integration/helper/posts_helper.dart
+++ lib/API Integration/helper/posts_helper.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_pratice/API%20Integration/model/posts.dart';
+import 'package:http/http.dart' as http;
 
 class PostHelper {
-  Future<List<Posts>>? getPosts() {}
+  Future<List<Posts>>? getPosts() {
+    http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  }
 }
