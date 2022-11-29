edb0c2532f62e5542b6cfe935e237132b5736f38
sadman_ichha <sadmanichha1@gmail.com>
2022-11-29 21:11:16 +0600

    http

--- lib/API Integration/user_post_screen.dart
+++ lib/API Integration/user_post_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pratice/API%20Integration/helper/posts_helper.dart';
+import 'package:flutter_pratice/API%20Integration/model/posts.dart';
 
 class UserPostScreen extends StatelessWidget {
-  const UserPostScreen({super.key});
+  List<Posts>? post;
+
+  getData() async {
+    var posts = await PostHelper().getPosts();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(itemCount:  ,itemBuilder: itemBuilder),
+      body: ListView.builder(
+          itemCount: post!.length,
+          itemBuilder: (_, index) {
+            return ListTile(
+              title: Text(post![index].title.toString()),
+            );
+          }),
     );
   }
 }
