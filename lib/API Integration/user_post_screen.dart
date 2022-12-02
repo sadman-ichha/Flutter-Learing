7a5c57c45307c42e85b178552854b7eda37548ee
sadman_ichha <sadmanichha1@gmail.com>
2022-12-02 21:23:43 +0600

    https

--- lib/API Integration/user_post_screen.dart
+++ lib/API Integration/user_post_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pratice/API%20Integration/helper/posts_helper.dart';
 import 'package:flutter_pratice/API%20Integration/model/posts.dart';
@@ -10,9 +12,13 @@ class UserPostScreen extends StatefulWidget {
 
 class _UserPostScreenState extends State<UserPostScreen> {
   List<Posts>? post = [];
+  bool isVisible = false;
 
   getData() async {
     post = await PostHelper().getPosts();
+    setState(() {
+      isVisible = true;
+    });
   }
 
   @override
@@ -31,19 +37,26 @@ class _UserPostScreenState extends State<UserPostScreen> {
       body: ListView.builder(
           itemCount: post!.length,
           itemBuilder: (_, index) {
-            return Card(
-              elevation: 5,
-              child: ListTile(
-                title: Text(post![index].title.toString()),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => PostDetailsScreen(post![index].userId,
-                          post![index].id, post![index].body),
-                    ),
-                  );
-                },
+            return Visibility(
+              visible: isVisible,
+              replacement: CircularProgressIndicator(),
+              child: Card(
+                elevation: 5,
+                child: ListTile(
+                  title: Text(post![index].title.toString()),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PostDetailsScreen(
+                          post![index].userId,
+                          post![index].id,
+                          post![index].body,
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             );
           }),
