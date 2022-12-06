e1d5d705386cbfc797087f65a65a68abce23d97f
sadman_ichha <sadmanichha1@gmail.com>
2022-12-07 00:04:05 +0600

    sharedPref

--- lib/shared preferences/shared_pref_demo.dart
+++ lib/shared preferences/shared_pref_demo.dart
@@ -1,19 +1,59 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class SharedPreferencesDemo extends StatelessWidget {
-  const SharedPreferencesDemo({super.key});
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesDemo extends StatefulWidget {
+  SharedPreferencesDemo({super.key});
+
+  @override
+  State<SharedPreferencesDemo> createState() => _SharedPreferencesDemoState();
+}
+
+class _SharedPreferencesDemoState extends State<SharedPreferencesDemo> {
+  int counter = 0;
+
+  Future increment() async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    setState(() {
+      counter++;
+      sharedPref.setInt("count", counter);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
+        children: [
           Center(
               child: Text(
-            "Data",
+            "You have Clicked $counter Times",
             style: TextStyle(fontSize: 20),
           )),
+          SizedBox(height: 30.0),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("+"),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15))),
+              ),
+              SizedBox(width: 35.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("-"),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15))),
+              ),
+            ],
+          ),
         ],
       ),
     );
