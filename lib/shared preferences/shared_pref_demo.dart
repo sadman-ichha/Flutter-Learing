2f89d9fb18040694da20549cf30aec943166e490
sadman_ichha <sadmanichha1@gmail.com>
2022-12-06 23:19:41 +0600

    shared pref

--- lib/shared preferences/shared_pref_demo.dart
+++ lib/shared preferences/shared_pref_demo.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SharedPreferencesDemo  extends StatelessWidget {
+class SharedPreferencesDemo extends StatelessWidget {
   const SharedPreferencesDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Center(child: Text("Data")),
-        
-      ],),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Center(
+              child: Text(
+            "Data",
+            style: TextStyle(fontSize: 20),
+          )),
+        ],
+      ),
     );
   }
 }
