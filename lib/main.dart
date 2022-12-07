898eefefeea97257a98c25448923eec81fd8e31f
sadman_ichha <sadmanichha1@gmail.com>
2022-12-07 11:22:22 +0600

    shared pref

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       home: SharedPreferencesDemo(),
     );
   }
